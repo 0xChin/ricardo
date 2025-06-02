afb547c72042b520d3616fe2a7ff66e6152a57be
0xchin <77933451+0xChin@users.noreply.github.com>
2025-06-02 01:06:55 -0300

    feat: make bot join voice channel

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
 // index.js
 import dotenv from 'dotenv';
 import { Client, GatewayIntentBits } from 'discord.js';
+import { joinVoiceChannel } from '@discordjs/voice';
 
 dotenv.config();
 
 const client = new Client({
-  intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildVoiceStates]
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildVoiceStates,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent
+  ]
+});
+
+client.on('messageCreate', async (message) => {
+  if (message.author.bot) return;
+  if (message.content !== '!join') return;
+
+  const voiceChannel = message.member.voice.channel;
+  if (!voiceChannel) return;
+
+  joinVoiceChannel({
+    channelId: voiceChannel.id,
+    guildId: voiceChannel.guild.id,
+    adapterCreator: voiceChannel.guild.voiceAdapterCreator
+  });
 });
 
 client.once('ready', () => {
